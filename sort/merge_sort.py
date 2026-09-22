e7a535eed245d94f73b5eeb3da3f5843a2581c4a
22178384 <22178384@qq.com>
2026-09-22 10:58:06 +0800

    Add sort/merge_sort.py

---
+++ sort/merge_sort.py
@@ -0,0 +1,21 @@
+"""归并排序（由练习补充）。"""
+
+
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    mid = len(arr) // 2
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+    out, i, j = [], 0, 0
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            out.append(left[i]); i += 1
+        else:
+            out.append(right[j]); j += 1
+    out.extend(left[i:]); out.extend(right[j:])
+    return out
+
+
+if __name__ == "__main__":
+    print(merge_sort([5, 2, 9, 1, 5, 6]))
